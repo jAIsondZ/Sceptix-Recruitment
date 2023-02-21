4fb3564d54dec37313f4178a149564faa32e6add
jAIsondZ <125900741+jAIsondZ@users.noreply.github.com>
2023-02-21 22:32:58 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,38 @@
+const expandable = document.querySelector('.expandable');
+const content = expandable.querySelector('.content');
+
+expandable.addEventListener('click', function() {
+  expandable.classList.toggle('active');
+});
+
+
+const expandable2 = document.querySelector('.expandable2');
+const content2 = expandable2.querySelector('.content2');
+
+expandable2.addEventListener('click', function() {
+  expandable2.classList.toggle('active');
+});
+
+
+const expandable3 = document.querySelector('.expandable3');
+const content3 = expandable.querySelector('.content3');
+
+expandable3.addEventListener('click', function() {
+  expandable3.classList.toggle('active');
+});
+
+
+const expandable4 = document.querySelector('.expandable4');
+const content4 = expandable4.querySelector('.content4');
+
+expandable4.addEventListener('click', function() {
+  expandable4.classList.toggle('active');
+});
+
+
+const expandable5 = document.querySelector('.expandable5');
+const content5 = expandable5.querySelector('.content5');
+
+expandable5.addEventListener('click', function() {
+  expandable5.classList.toggle('active');
+});
